000ae5fc6685acabf27ed5ccba9063def8ac358d
Leoj_A <jeremykaizer@gmail.com>
2022-03-20 10:54:34 -0400

    Add imgPreview, refreshCSS, getContrast

--- domFunctions.js
+++ domFunctions.js
@@ -99,4 +99,67 @@ const createElement = (place, domElement, ...args) => {
     }
 }
 
-export {_, deleteChild, createElement};
+//Devuelve blanco o negro dependiendo del color de entrada
+const getContrast = function (hexcolor){
+
+	// If a leading # is provided, remove it
+	if (hexcolor.slice(0, 1) === '#') {
+		hexcolor = hexcolor.slice(1);
+	}
+
+	// If a three-character hexcode, make six-character
+	if (hexcolor.length === 3) {
+		hexcolor = hexcolor.split('').map(function (hex) {
+			return hex + hex;
+		}).join('');
+	}
+
+	// Convert to RGB value
+	var r = parseInt(hexcolor.substr(0,2),16);
+	var g = parseInt(hexcolor.substr(2,2),16);
+	var b = parseInt(hexcolor.substr(4,2),16);
+
+	// Get YIQ ratio
+	var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+	// Check contrast
+	return (yiq >= 128) ? 'black' : 'white';
+
+};
+
+// Previsualizar la imagen seleccionada
+const imgPreview = function (inputId, imgId){
+    const inputSelect = _(inputId),
+        imgPrev = _(imgId);
+    
+    inputSelect.addEventListener('change', function(){
+        const files = inputSelect.files;
+
+        if(!files || !files.length){
+            imgPrev.src = '';
+            return;
+        }
+
+        const firstFile = files[0];
+        const objectUrl = URL.createObjectURL(firstFile);
+        imgPrev.src = objectUrl;
+    });
+
+}
+
+const refreshCSS = () => {
+    let links = document.getElementsByTagName('link');
+    for (let i = 0; i < links.length; i++) {
+        if (links[i].getAttribute('rel') == 'stylesheet') {
+            let href = links[i].getAttribute('href')
+                                    .split('?')[0];
+            
+            let newHref = href + '?version=' 
+                        + new Date().getMilliseconds();
+            
+            links[i].setAttribute('href', newHref);
+        }
+    }
+}
+
+export {_, deleteChild, createElement, getContrast, imgPreview, refreshCSS};
